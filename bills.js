d22952ed622bf7748c3eb1fd0ef03323c8bfa610
Julia Palacios <moreparaya@gmail.com>
2020-12-13 17:45:14 +0000

    change of name bill.js

--- bills.js
+++ bills.js
@@ -9,6 +9,6 @@ contactSchema.methods.cleanup = function(){
     return {name: this.name, phone: this.phone};
 }
 
-const Contact = mongoose.model('Contact', contactSchema);
+const Bill = mongoose.model('Bill', contactSchema);
 
-module.exports = Contact;
+module.exports = Bill;
