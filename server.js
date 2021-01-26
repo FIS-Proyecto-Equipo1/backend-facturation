8784fd0d0ae095d9db36f01074fa83ee808e1f43
Julia Palacios <moreparaya@gmail.com>
2021-01-26 15:27:57 +0000

    integration fix3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const Bill = require ('./bills');
+const Bill = require('./bills');
 const { updateOne, findByIdAndUpdate } = require('./bills');
 const VehiclesResource = require('./vehiclesResource');
 const UsersResource = require('./usersResource');
@@ -10,11 +10,11 @@ var BASE_API_PATH = "/api/v1";
 
 var app = express();
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 app.use(bodyParser.json());
 
@@ -22,60 +22,60 @@ app.get("/", (req, res) => {
     res.send("<html><body><h1>FACTURATION</h1></body></html>");
 });
 
-function randomBillNumber(){
-    let bill_Number = "US" + getRandomArbitrary(00000,99999);
+function randomBillNumber() {
+    let bill_Number = "US" + getRandomArbitrary(00000, 99999);
     console.log(bill_Number);
     return bill_Number;
 }
 
 function getRandomArbitrary(min, max) {
     return parseInt(Math.random() * (max - min) + min);
-  }
+}
 
-app.get(BASE_API_PATH + "/bills", (req,res) => {
+app.get(BASE_API_PATH + "/bills", (req, res) => {
     idCliente = req.header('x-user');
     console.log(`user: ${idCliente}`);
     console.log(Date() + " - GET /bills");
-    
+
     Bill.find(req.query, (err, bills) => {
-        if(err){
-            console.log(Date()+" - "+ err);
+        if (err) {
+            console.log(Date() + " - " + err);
             res.sendStatus(500);
-        }else{
+        } else {
             console.log(Date() + " GET /bills")
             res.send(bills.map((bill) => {
-                return bill.cleanup();}));
+                return bill.cleanup();
+            }));
         }
     })
 });
 
 
-app.get(BASE_API_PATH + "/bills/generatePdf/:billNumber", (req,res) => {
-    Bill.findOne({"billNumber":req.params.billNumber}, (err, bill) => {
-        if(err){
-            console.log(Date()+" - "+ err);
+app.get(BASE_API_PATH + "/bills/generatePdf/:billNumber", (req, res) => {
+    Bill.findOne({ "billNumber": req.params.billNumber }, (err, bill) => {
+        if (err) {
+            console.log(Date() + " - " + err);
             res.sendStatus(500);
-        }else{
-            if(bill == null){
-                console.log(Date() + " GET /bills/ "+req.params.billNumber +" - Invalid");
+        } else {
+            if (bill == null) {
+                console.log(Date() + " GET /bills/ " + req.params.billNumber + " - Invalid");
                 res.sendStatus(404);
             }
-            else    
-            {
+            else {
                 const pdf = require('html-pdf');
                 var fs = require('fs');
                 const fileName = 'pdf.pdf'
                 const content = '<h1>Factura: ' + bill.billNumber + '</h1>' + '<p><strong>Nombre: </strong>' + bill.name + ' ' + bill.surname + '</p>'
-                + '<p><strong>Vehículo: </strong>' + bill.vehicle + '</p>' + '<p><strong>Duración: </strong>' + bill.duration + '</p>'
-                + '<p><strong>Importe: </strong>' + parseFloat(Math.round(bill.amount)).toFixed(2) + "€" + '</p>' + '<p><strong>Estado: </strong>' + bill.billStatus + '</p>' 
-            
-                pdf.create(content).toBuffer(function(err, buffer) {
-                    if (err){
+                    + '<p><strong>Vehículo: </strong>' + bill.vehicle + '</p>' + '<p><strong>Duración: </strong>' + bill.duration + '</p>'
+                    + '<p><strong>Importe: </strong>' + parseFloat(Math.round(bill.amount)).toFixed(2) + "€" + '</p>' + '<p><strong>Estado: </strong>' + bill.billStatus + '</p>'
+
+                pdf.create(content).toBuffer(function (err, buffer) {
+                    if (err) {
                         console.log(err);
                     } else {
                         var savedFilePath = './tmp/' + fileName;
                         console.log(buffer)
-                        fs.writeFile(savedFilePath, buffer, function() {
+                        fs.writeFile(savedFilePath, buffer, function () {
                             res.status(200).download(savedFilePath, fileName);
                         });
                     }
@@ -85,47 +85,46 @@ app.get(BASE_API_PATH + "/bills/generatePdf/:billNumber", (req,res) => {
     })
 });
 
-app.get(BASE_API_PATH + "/bills/billStatus/:billStatus", (req, res)  => {
-    Bill.find({"billStatus": req.params.billStatus}, (err, bills) => {
-        if(err){
-            console.log(Date()+" - "+ err);
+app.get(BASE_API_PATH + "/bills/billStatus/:billStatus", (req, res) => {
+    Bill.find({ "billStatus": req.params.billStatus }, (err, bills) => {
+        if (err) {
+            console.log(Date() + " - " + err);
             res.sendStatus(500);
-        }else{
-            if(req.params.billStatus != 'Pagado' && req.params.billStatus != 'No pagado'){
-                console.log(Date() + " GET /bills/"+req.params.billStatus +" - Invalid");
+        } else {
+            if (req.params.billStatus != 'Pagado' && req.params.billStatus != 'No pagado') {
+                console.log(Date() + " GET /bills/" + req.params.billStatus + " - Invalid");
                 res.sendStatus(404);
             }
-            else    
-            {
-                console.log(Date() + " GET /bills/"+req.params.billStatus);
+            else {
+                console.log(Date() + " GET /bills/" + req.params.billStatus);
                 res.send(bills.map((bill) => {
-                    return bill.cleanup();}));
+                    return bill.cleanup();
+                }));
 
             }
         }
     })
 });
 
-app.get(BASE_API_PATH + "/bills/:billNumber", (req, res)  => {
-    Bill.findOne({"billNumber":req.params.billNumber}, (err, bill) => {
-        if(err){
-            console.log(Date()+" - "+ err);
+app.get(BASE_API_PATH + "/bills/:billNumber", (req, res) => {
+    Bill.findOne({ "billNumber": req.params.billNumber }, (err, bill) => {
+        if (err) {
+            console.log(Date() + " - " + err);
             res.sendStatus(500);
-        }else{
-            if(bill == null){
-                console.log(Date() + " GET /bills/ "+req.params.billNumber +" - Invalid");
+        } else {
+            if (bill == null) {
+                console.log(Date() + " GET /bills/ " + req.params.billNumber + " - Invalid");
                 res.sendStatus(404);
             }
-            else    
-            {
-                console.log(Date() + " GET /bills/ "+req.params.billNumber);
+            else {
+                console.log(Date() + " GET /bills/ " + req.params.billNumber);
                 res.send(bill.cleanup())
             }
         }
     })
 });
 
-app.post(BASE_API_PATH + "/bills",(req, res) => {
+app.post(BASE_API_PATH + "/bills", (req, res) => {
     console.log(Date() + " - POST /bills");
     var bill = req.body;
     idCliente = req.header('x-user')
@@ -133,70 +132,66 @@ app.post(BASE_API_PATH + "/bills",(req, res) => {
     //Asignamos billNumber
     bill.billNumber = randomBillNumber();
 
-     //Obtenemos los datos del usuario
-     UsersResource.getUser(bill.id_client)
-     .then((user => {
-    
-        bill.name = user.nombre;
-        bill.surnames = user.apellidos;
-    
-    }));
+    //Obtenemos los datos del usuario
+    UsersResource.getUser(bill.id_client)
+        .then((user => {
+            console.log(user);
+            bill.name = user.nombre;
+            bill.surnames = user.apellidos;
 
-     // Obtenemos los datos del vehiculo
-     VehiclesResource.getVehicle(bill.id_vehicle)
-     .then((vehicle_type =>{
-         bill.vehicle = vehicle_type.tipo;
-     }));
 
-    Bill.create(bill, (err) => {
-        if (err){
-            console.log(Date() + "-" + err);
-            res.sendStatus(500);
+            // Obtenemos los datos del vehiculo
+            VehiclesResource.getVehicle(bill.id_vehicle)
+                .then((vehicle_type => {
+                    bill.vehicle = vehicle_type.tipo;
+
+                    Bill.create(bill, (err) => {
+                        console.log(bill);
+                        if (err) {
+                            console.log(Date() + "-" + err);
+                            res.sendStatus(500);
+                        } else {
+                            res.sendStatus(201);
+                        }
+                    });
+                }));
+        }));
+});
+
+app.delete(BASE_API_PATH + "/bills/:billNumber", (req, res) => {
+    let billNumber = req.params.billNumber;
+
+    Bill.findOneAndDelete({ "billNumber": billNumber }, (err, billDelete) => {
+        if (err == null && billDelete == null) {
+            var auxErr = new Error("Bill not found " + billNumber);
+            console.log(Date() + " - " + auxErr);
+            res.sendStatus(404)
+        }
+        else if (err) {
+            console.log(err);
+            res.sendStatus(500)
         } else {
-            res.sendStatus(201);
+            console.log(Date() + " DELETE /bills/" + billNumber)
+            res.status(204).send({ message: "Bill " + billNumber + " deleted" });
         }
     });
 });
 
-app.delete(BASE_API_PATH + "/bills/:billNumber", (req, res)  => {
-        let billNumber = req.params.billNumber;
-
-        Bill.findOneAndDelete({"billNumber": billNumber}, (err, billDelete) => {
-            if(err == null && billDelete == null)
-            {    
-                var auxErr = new Error("Bill not found " + billNumber);
-                console.log(Date()+" - "+auxErr);
-                res.sendStatus(404)
-            }
-            else if(err)
-            {    
-                console.log(err);
-                res.sendStatus(500)
-            }else
-            {
-                console.log(Date() + " DELETE /bills/" + billNumber)
-                res.status(204).send({message : "Bill " + billNumber+ " deleted"});
-            }
-        });  
-});
-
-app.put(BASE_API_PATH + "/bills/:billNumber",(req, res) => {
+app.put(BASE_API_PATH + "/bills/:billNumber", (req, res) => {
     console.log(Date() + " - PUT /bills/billNumber={billNumber}");
     let billNumber = req.params.billNumber;
     let update_bill = req.body;
-    Bill.findOneAndUpdate({"billNumber": billNumber}, update_bill, { runValidators: true }, (err, bill_update) => {
-        if(err == null && bill_update == null)
+    Bill.findOneAndUpdate({ "billNumber": billNumber }, update_bill, { runValidators: true }, (err, bill_update) => {
+        if (err == null && bill_update == null)
             err = new Error("Bill not found " + billNumber);
-        if(err)
-        {    
-            console.log(Date()+" - "+err);
+        if (err) {
+            console.log(Date() + " - " + err);
             res.sendStatus(500);
-        }else
-        {
+        } else {
             console.log(Date() + " PUT /bills/" + billNumber);
-            res.status(200).send({billNumber : bill_update, body: req.body});
-        }                                                                     
-                                                                              
+            res.status(200).send({ billNumber: bill_update, body: req.body });
+        }
+
     });
 });
 
